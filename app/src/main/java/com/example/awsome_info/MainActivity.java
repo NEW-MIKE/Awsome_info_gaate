1ff529d09bd8a7b99d05a499d6652c4aac417805
jj <jj@email.com>
2020-06-07 17:37:53 +0800

    test

--- app/src/main/java/com/example/awsome_info/MainActivity.java
+++ app/src/main/java/com/example/awsome_info/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        int i;
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
